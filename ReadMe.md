8f2e4f467dbdbbdc3883baa3bc617c095f3745bb
srisubaramb <srisubaramb@gmail.com>
2026-02-02 12:51:20 +0530

    day 0 backend understanding part 1

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+Understanding backend fundamentals with Node.js:
+Day 0:
+- Request–response lifecycle
+- Project initialization (npm, package.json)
+- Creating an HTTP server
+- Ports and server listening
+- HTTP methods (GET)
+- Method-based routing
+- URL parsing using the URL object
+- Pathname and query parameters
+- Control flow using URL data
+- API testing using Postman
---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const PORT = 3000
+const app = http.createServer((req , res) => {
+	const parsedUrl = new URL(req.url, "http://" + req.headers.host)
+	console.log(parsedUrl)
+	if (req.method == "GET") {
+		if (parsedUrl.pathname == "/health") {
+			res.statusCode = 200;
+			res.end("Server number "+ parsedUrl.searchParams.get("no") + " is healthy")
+		}
+		else {
+			res.statusCode = 404
+			res.end("Page not found")
+		}
+	} else {
+		res.statusCode = 405
+		res.end("Request is not allowed")
+	}
+})
+app.listen(PORT)
+console.log("App listening at port: " + PORT)
